72964ef97f4535dd336d78d78ca92e20ba1eb698
Jose Aguilera <60164793+JAguilera3515@users.noreply.github.com>
2025-06-27 10:15:07 -0400

    Create Find CIrcuit speed in ( ).py

---
+++ Find CIrcuit speed in ( ).py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+test = xl("M7865")
+start = 0
+end = 0
+Flag = False
+for x in range(0,len(test)):
+    if test[x]=="(":
+        Flag = True
+        start = x+1
+    if test[x]=="s" and Flag==True:
+        end = x+1
+    if test[x]==")" and Flag==True:
+        break
+test[start:end]
